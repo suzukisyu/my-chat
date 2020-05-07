3ce853b35554202c16aec2e1ac1b7a13bfd421a5
EC2 Default User <ec2-user@ip-172-31-47-104.us-east-2.compute.internal>
2020-05-07 22:04:51 +0000

    デザインを変更

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,14 +1,14 @@
 <template>
-  <div>
+  <div class="background">
     <div v-for="(message, key) in messages" v-bind:key="key">
-      <div class="name">
+      <div class=" name">
         {{ message.name }}
       </div>
-      <div class="text">
+      <div class=" text">
         {{ message.text }}
       </div>
     </div>
-    <div class="input-form">
+    <div class=" input-form">
       <div>
         <input
           v-model="inputName"
@@ -29,7 +29,7 @@
         ></textarea>
       </div>
       <div>
-        <button v-on:click="onSubmit">送信</button>
+        <button class="button" v-on:click="onSubmit">送信</button>
       </div>
     </div>
   </div>
@@ -70,19 +70,84 @@ export default {
 </script>
 
 <style scoped>
-.name {
-  font-weight: bold;
-}
-.text {
+
+.name { 
+  margin-top: 5px;
   text-indent: 1em;
+  font-weight: bold;
+  background:#87cefa;
+  width: 100px;
+  height:30px;
+  border-radius: 50px;
 }
+
+
+.text {
+  margin-top: 5px;
+  text-indent: 2em;
+  background:#98fb98;
+  width: 200px;
+  height:30px;
+  border-radius: 50px;
+  
+}
+
+
 .input-form {
-  margin-top: 10px;
+  margin-top: 20px;
 }
+
+
 .input-name {
   width: 100px;
+  padding: 0.5em 1em;
+  text-decoration: none;
+  background: #f7f7f7;
+  border-left: solid 6px #87cefa;
+  color: #87cefa;
+  font-weight: bold;
+  box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.29);
 }
-.input-name {
-  width: 100px;
+
+.input-text{
+   margin-top: 5px;
+   width: 300px;
+ 
+  padding: 0.5em 1em;
+  text-decoration: none;
+  background: #f7f7f7;
+  border-left: solid 6px #98fb98;
+  color: #98fb98;
+  font-weight: bold;
+  box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.29);
 }
+
+
+.button{
+  display: inline-block;
+  padding: 0.5em 1em;
+  text-decoration: none;
+  background: #f7f7f7;
+  border-left: solid 6px #ff7c5c;/*左線*/
+  color: #ff7c5c;/*文字色*/
+  font-weight: bold;
+  box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.29);
+}
+.button:hover {
+  box-shadow: inset 0 0 2px rgba(128, 128, 128, 0.1);
+  transform: translateY(2px);
+}
+
+
+.background{
+  padding: 10px; 
+  margin-bottom: 10px; 
+  border: 5px 
+  double #333333
+}
+
+
+
+
+
 </style>
