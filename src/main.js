6fda8a5b1785474dde03481f079fbc344bf566c9
EC2 Default User <ec2-user@ip-172-31-47-104.us-east-2.compute.internal>
2020-05-07 23:17:17 +0000

    ブラウザ間で連携できるように変更

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBkX2fJ-0qZydsTn68QSV3AWXmsQhCO_5o",
+  authDomain: "my-chat-backend.firebaseapp.com",
+  databaseURL: "https://my-chat-backend.firebaseio.com",
+  projectId: "my-chat-backend",
+  storageBucket: "my-chat-backend.appspot.com",
+  messagingSenderId: "949948022301",
+  appId: "1:949948022301:web:25ce515a0e03a07cd190f4"
+}
+firebase.initializeApp(firebaseConfig)
 
 new Vue({
   render: h => h(App),
