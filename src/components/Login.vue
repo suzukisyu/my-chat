8d2360565d6f0960304e053b4f9328b9a267f899
mynameistarou <katayama.masaru@gmail.com>
2020-06-27 09:49:30 +0900

    戻るリンクを追加

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     ログイン<br/>
+    <router-link to="/">ログイン</router-link><br/>
     <router-link to="/signup">ユーザー登録</router-link><br/>
   </div>
 </template>
--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    ユーザー登録
+    ユーザー登録<br/>
+    <router-link to="/">ユーザー登録</router-link><br/>
+    <router-link to="/login">戻る</router-link><br/>
   </div>
 </template>
 
