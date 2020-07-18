2439616e3b9786d9920c7dd8ce12b4aab04882e0
Suzuki Syu <Syumai19991201@gmail.com>
2020-07-18 00:46:39 +0000

    20200711宿題

--- src/components/Create.vue
+++ src/components/Create.vue
@@ -1,7 +1,21 @@
 <template>
   <div>
-    チャットルーム作成<br/>
-    <router-link to="/">戻る</router-link><br/>
+    <h1 class="title">my-chat チャットルーム作成</h1><br/>
+    <div class="box">    
+        <div>
+          チャットルーム名<input type="text" class="room">
+        </div>  
+    
+        <div>
+          
+          <button class="button">作成</button>
+          
+        </div>
+        
+        <br>
+        <br>
+        <router-link to="/"class="back">戻る</router-link><br/>
+    </div>    
   </div>
 </template>
 
@@ -22,4 +36,38 @@ export default {
 </script>
 
 <style scoped>
+
+.box{
+  padding: 1em 1em;
+    margin: 1em 0;
+    font-weight: bold;
+    border: solid 1px #000000;
+}
+
+.title{
+  font-size:20px;
+}
+
+.room{
+  font-size:20px;
+  
+}
+
+
+.back{
+   padding: 0.5em 1em;
+    margin: 2em 0;
+    font-weight: bold;
+    border: solid 1px #000000;
+    right: 100%;
+}
+
+.button{
+   position: relative;
+  top: 10px;
+  left: 128px;
+  font-size:20px;
+  background:white;
+}
+
 </style>
--- src/components/List.vue
+++ src/components/List.vue
@@ -1,11 +1,22 @@
 <template>
-  <div>
-    チャットルーム一覧<br/>
-    <router-link to="/create">新規作成</router-link><br/>
-    <router-link to="/chat">チャットルーム</router-link><br/>
-    <router-link to="/login">ログアウト</router-link><br/>
-  </div>
-</template>
+  <div class="background">
+    
+      <h1 class="title">【チャットルーム一覧】</h1>
+      
+    <div class="box1">
+        
+      <h3 class="title2">my-chat　ホーム</h3>
+      <router-link to="/create" class="sinki">新規作成</router-link><br/>
+      <br>
+      <router-link to="/chat" class="chat">チャットルーム</router-link><br/>
+      <br>
+      <router-link to="/login" class="log">ログアウト</router-link>
+      <br/>
+      
+    </div>
+
+  </div >  
+  </template>
 
 <script>
 export default {
@@ -24,4 +35,51 @@ export default {
 </script>
 
 <style scoped>
+
+.title{
+  font-size: 20px;
+  font-weight: lighter;
+}
+
+.title2{
+  font-weight: bold;
+}
+
+  .background{
+    
+  }
+  
+  .box1 {
+    padding: 1em 1em;
+    margin: 1em 0;
+    font-weight: bold;
+    border: solid 1px #000000;
+  }
+  
+  .sinki{
+    padding: 0.5em 1em;
+    margin: 2em 0;
+    font-weight: bold;
+    border: solid 1px #000000;
+  }
+  
+  .chat{
+    padding: 0.5em 1em;
+    margin: 2em 0;
+    font-weight: bold;
+    border: solid 1px #000000;
+  }
+  
+  .log{
+   
+   padding: 0.5em 1em;
+    margin: 2em 0;
+    font-weight: bold;
+    border: solid 1px #000000;
+    right: 100%;
+  }
+  
+
 </style>
+
+
